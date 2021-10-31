77508cb4fdb92d4686518204b712dc3a3117a33c
ELENA-MARIA HARANGUȘ <harangusemaria@gmail.com>
2021-10-31 20:23:58 +0200

    Am adaugat lab4

---
+++ StopCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Harangus_Maria_Lab2.CustomCommands
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommand()
+        {
+            InputGestureCollection myInputGesture = new InputGestureCollection();
+            myInputGesture.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(StopCommand), myInputGesture);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
